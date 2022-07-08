fae6ecc2edbe0a54c9b34458ff6fefd32b7ec6a6
Maninaid <yvmanikanta1999@gmail.com>
2022-07-08 21:49:34 +0530

    FinalProject

---
+++ FinalProject/FinalProject/Models/Product.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FinalProject.Models
+{
+    public class Product
+    {
+        [Key]
+        public int ProductID { get; set; }
+        [Required]
+        public string ProductName { get; set; }
+        public float Price { get; set; }
+        public string Brand { get; set; }
+        public DateTime ManufactureDate { get; set; }
+        public DateTime ExpirationDate { get; set; }
+    }
+}
